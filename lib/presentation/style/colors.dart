43e2f17f2a6f94dce85945b05eea7da1f46fac33
Franz <francesco.romeo-work@libero.it>
2022-10-18 10:52:50 +0200

    Colors Addition
    Added color for correct and wrong answer

--- lib/presentation/style/colors.dart
+++ lib/presentation/style/colors.dart
@@ -6,4 +6,6 @@ const backgroundBottomColor = Color.fromARGB(255, 65, 102, 225);
 const questionColor = Color(0xFFFFFFFF);
 const questionIndexColor = Color.fromARGB(255, 204, 224, 245);
 const answerButtonColor = Color(0xFFFFFFFF);
+const answerButtonColorCorrect = Color(0xFF00CC00);
+const answerButtonColorWrong = Color(0xFFBD0000);
 const answerButtonTextColor = Color.fromARGB(255, 52, 84, 223);
